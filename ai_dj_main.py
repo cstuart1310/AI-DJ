9183534b0d41463e50b3b24e76d5c088d7759346
cstuart1310 <cstuart1310@gmail.com>
2024-06-30 17:11:17 +0100

    Fixed song length counter
    Now correctly only goes up to set limit of songs

--- ai_dj_main.py
+++ ai_dj_main.py
@@ -100,10 +100,10 @@ if shuffleSongs==True:
 
 if songLength==None or songLength>len(songs):#If song length is default value or greater than allowed
     songLength=len(songs)#replaces None with length of array so all songs are iterated through
-
+    
 for songIndex, songFile in enumerate(songs[0:songLength]):#iterates through the chosen number of songs
         if os.path.splitext(songFile)[1].lower() in audioExtensions:#checks to make sure file is a valid audio file (Also filters out subdirs)
-            if songIndex+intermissions<len(songs):#while there is songs enough for another intermission
+            if songIndex+intermissions<len(songs) and songIndex+1<songLength:#while there is songs enough for another intermission, and not too many to be out of the range
                 songX, nameX = (getTags(songFile))#tags of songindex
                 songY, nameY = (getTags(songs[songIndex+1]))#tags of next in list after index
                 if all(noneCheck is not None for noneCheck in [songX,songY,nameX,nameY]):#if all vars have a non-none value
@@ -114,10 +114,11 @@ for songIndex, songFile in enumerate(songs[0:songLength]):#iterates through the 
                     with open(transitionsFile,"a") as transitionFile:
                         transitionFile.write((songX+"|"+songY+"|"+nameX+"|"+nameY+"|"+transitionName+"\n"))#writes song names and artist names to txt for passing to other script
             else:#final song in array or final song that can fit a transition
-                for songFile in songs[songIndex:]:#Loops through all remaining songs
-                    print("Can't fit any more transitions, adding",songFile,"to playback list")
-                    playbackOrder.append(songFile)#adds songs to playback order without generating transitions
-                break#breaks the main loop of songs
+                print("Can't fit any more transitions/songs, adding",songFile,"to playback list")
+                playbackOrder.append(songFile)#adds songs to playback order without generating transitions
+                
+
+ai_dj_audio.printPlaybackOrder(playbackOrder)
 subprocess.run(("python3 "+cwd+"/ai_dj_text.py"),shell=True)#Runs the text generator via subprocess. Subprocess needed due to memory leak if func called then "cleaned up". Text generated is outputted to txt
 
 print("_"*30)
